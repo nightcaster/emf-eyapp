3104db0e97b775a9f813430026c04205b557453e
Mat Booth <mat.booth@gmail.com>
2026-04-11 18:12:30 +0100

    Make new greetings easier to add
    Added some more greetings
    
    Bump version number

--- app.py
+++ app.py
@@ -20,6 +20,40 @@ ppi = display_x / display_height_inches
 # Font size
 one_pt = ppi / 72
 
+start_greets = [
+    "Ey Up",
+    "How Do",
+    "Now Then",
+    "Wotcha",
+    "Alreet",
+    "G'day",
+    "Ar kid",
+    "<T>",
+    "<T> is it?",
+]
+intro_greets = [
+    "<N> innit",
+    "Ahm <N>",
+    "I'm <N>",
+    "Name's <N>",
+    "<N> 'ere",
+    "It's <N>",
+]
+end_greets = [
+    "Ow a tha?",
+    "Ah thee?",
+    "Orate?",
+    "Yareet?",
+    "Fettlin well?",
+    "Ow's tha deein?",
+    "Put kettle on",
+    "Tha mashin?",
+    "Get mashin",
+    "Went a brew?",
+    "Gerron wi yer",
+    "Wot's fer tea?",
+]
+
 class EyApp(app.App):
 
     def __init__(self):
@@ -27,7 +61,7 @@ class EyApp(app.App):
         self.button_states = Buttons(self)
         self.name = settings.get("name")
         if not self.name:
-            self.name = "<yobbo>"
+            self.name = "Yobbo"
         self.elapsed = 0
         self.text_accum = 0
         self.chaos = self._load()
@@ -123,9 +157,9 @@ class EyApp(app.App):
         if self.text_accum > self.text_delay:
             self.text_accum = self.text_accum - self.text_delay
             if self.chaos > 0:
-                self.greet0 = random.randrange(7)
-                self.greet1 = random.randrange(4)
-                self.greet2 = random.randrange(4)
+                self.greet0 = random.randrange(len(start_greets))
+                self.greet1 = random.randrange(len(intro_greets))
+                self.greet2 = random.randrange(len(end_greets))
 
         # Choose new text colour
         if self.chaos > 0:
@@ -197,56 +231,33 @@ class EyApp(app.App):
 
         ctx.font_size = self.main_font_size * one_pt
 
+        t = time.localtime()[3];
+        time_greet = "Eeevenin"
+        if t >= 0 and t < 12:
+            time_greet = "Mooornin"
+        if t >= 12 and t < 17:
+            time_greet = "Aaafternoon"
+
         x_factor = math.cos(math.radians(self.elapsed % 360)) * ratio
         y_factor = math.sin(math.radians(self.elapsed % 360)) * ratio
         offset = (20 * x_factor) + random.randrange(1 + 2 * self.chaos)
         offsety = -20 + (5 * y_factor) + random.randrange(1 + 1 * self.chaos)
-        if self.greet0 == 0:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Ey Up")
-        if self.greet0 == 1:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("How Do")
-        if self.greet0 == 2:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Now Then")
-        if self.greet0 == 3:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Wotcha")
-        if self.greet0 == 4:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Alreet")
-        if self.greet0 == 5:
-            t = time.localtime()[3];
-            if t >= 0 and t < 12:
-                ctx.rgb(*self.col).move_to(offset, offsety).text("Mooornin")
-            if t >= 12 and t < 17:
-                ctx.rgb(*self.col).move_to(offset, offsety).text("Aaafternoon")
-            if t >= 17:
-                ctx.rgb(*self.col).move_to(offset, offsety).text("Eeevenin")
-        if self.greet0 == 6:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("G'day")
+        start_greet = start_greets[self.greet0].replace("<T>", time_greet)
+        ctx.rgb(*self.col).move_to(offset, offsety).text(start_greet)
 
         x_factor = math.cos(math.radians((self.elapsed % 360) + 120)) * ratio
         y_factor = math.sin(math.radians((self.elapsed % 360) + 120)) * ratio
         offset = (20 * x_factor) + random.randrange(1 + 2 * self.chaos)
         offsety = 10 + (5 * y_factor) + random.randrange(1 + 1 * self.chaos)
-        if self.greet1 == 0:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Ahm " + self.name)
-        if self.greet1 == 1:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("I'm " + self.name)
-        if self.greet1 == 2:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Name's " + self.name)
-        if self.greet1 == 3:
-            ctx.rgb(*self.col).move_to(offset, offsety).text(self.name + " 'ere")
+        intro_greet = intro_greets[self.greet1].replace("<N>", self.name)
+        ctx.rgb(*self.col).move_to(offset, offsety).text(intro_greet)
 
         x_factor = math.cos(math.radians((self.elapsed % 360) + 240)) * ratio
         y_factor = math.sin(math.radians((self.elapsed % 360) + 240)) * ratio
         offset = (20 * x_factor) + random.randrange(1 + 2 * self.chaos)
         offsety = 40 + (5 * y_factor) + random.randrange(1 + 1 * self.chaos)
-        if self.greet2 == 0:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Ow a tha?")
-        if self.greet2 == 1:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Ah thee?")
-        if self.greet2 == 2:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Orate?")
-        if self.greet2 == 3:
-            ctx.rgb(*self.col).move_to(offset, offsety).text("Yareet?")
+        end_greet = end_greets[self.greet2].replace("<T>", time_greet)
+        ctx.rgb(*self.col).move_to(offset, offsety).text(end_greet)
 
         ctx.font_size = self.level_font_size * one_pt
         ctx.rgb(1, 1, 1).move_to(0, 100).text(str(self.chaos))
